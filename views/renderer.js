103e959c0b7254e6896a9ed7b20a184bacc093ed
leandro <leandro@eudaimonia.com.ar>
2018-11-16 17:47:18 -0300

    all buttons and top menu functions

--- views/renderer.js
+++ views/renderer.js
@@ -11,9 +11,22 @@ var vueApp = new Vue({
 	el: '#vueApp',
 	data: {
 		currentLocation: config.homeUri,
-		showMenuBar: false
+		showMenuBar: false,
+		loading: false
 	},
 	methods: {
-
+		maximize: function() {
+			if(remote.BrowserWindow.getFocusedWindow().isMaximized()){
+				remote.BrowserWindow.getFocusedWindow().unmaximize();
+			} else {
+				remote.BrowserWindow.getFocusedWindow().maximize();
+			}
+		},
+		minimize: function() {
+			remote.BrowserWindow.getFocusedWindow().minimize();
+		},
+		exit: function() {
+			remote.app.quit();
+		},
 	}
 });
