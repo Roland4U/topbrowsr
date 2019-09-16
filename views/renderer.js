bc76db6378a1f64c26faca548729ec01120576dc
leandro <leandro@eudaimonia.com.ar>
2019-09-16 12:26:59 -0300

    bookmarks

--- views/renderer.js
+++ views/renderer.js
@@ -2,10 +2,13 @@ const { remote } = require('electron');
 const { Menu, BrowserWindow, MenuItem, shell } = remote;
 const fs = require("fs");
 
-var webview;
+var webview,
+    bookmarks = [];
 
 onload = () => {
     webview = document.querySelector('webview');
+
+    // ********************************* TOOLBAR EVENTS *********************************
 
     $("#url").keyup(function (e) {
         if (e.keyCode == 13) {
@@ -36,6 +39,23 @@ onload = () => {
         webview.stop();
     });
 
+    $(".addBookmark").click(function () {
+        addBookmark();
+        refreshBookmarks();
+    });
+
+    $("#bookmarkList").delegate("li", "click", function (e) {
+        let index = $('li').index(this) - 1;
+        if (index == -1) {
+            return;
+        }
+        if ($(e.target)[0].localName == "i") {
+            deleteBookmark(index);
+        } else {
+            loadURL(bookmarks[index].url);
+        }
+    });
+
     $("#opacity").change(function () {
         let opacityValue = $(this).val() / 100;
         changeOpacity(opacityValue);
@@ -45,6 +65,8 @@ onload = () => {
         enableClickThrough();
     });
 
+    // ********************************* WEBVIEW EVENTS *********************************
+
     webview.addEventListener('did-start-loading', (e) => {
         showStop();
     });
@@ -52,7 +74,6 @@ onload = () => {
     webview.addEventListener('did-stop-loading', (e) => {
         showRefresh();
     });
-
 
     webview.addEventListener('did-navigate', (e) => {
         $("#url").val(e.url);
@@ -62,7 +83,8 @@ onload = () => {
         $("#url").val(e.url);
     });
 
-}
+    refreshBookmarks();
+};
 
 function showRefresh() {
     $("#reload").show();
@@ -90,4 +112,49 @@ function enableClickThrough() {
 
 function changeOpacity(opacity) {
     $("body").css('opacity', opacity);
+}
+
+function addBookmark() {
+    for (let i = 0; i < bookmarks.length; i++) {
+        if (webview.getURL() == bookmarks[i].url)
+            return;
+    }
+    bookmarks.push({
+        url: webview.getURL(),
+        title: webview.getTitle()
+    });
+    saveBookmark();
+}
+
+function saveBookmark(callback) {
+    fs.writeFile("./bookmarks.json", JSON.stringify(bookmarks), (err) => {
+        if (err) {
+            console.log("An error ocurred creating the file " + err.message);
+        } else {
+            if (callback)
+                callback();
+        }
+    });
+}
+
+function deleteBookmark(i) {
+    bookmarks.splice(i, 1);
+    saveBookmark(() => {
+        refreshBookmarks();
+    });
+}
+
+function refreshBookmarks() {
+    fs.readFile("./bookmarks.json", 'utf-8', (err, data) => {
+        $("li").remove(".bookmark");
+        if (err) {
+            bookmarks = [];
+            console.log("There was an error loading bookmarks", err);
+        }
+        bookmarks = JSON.parse(data);
+
+        for (let i = 0; i < bookmarks.length; i++) {
+            $('#bookmarkList').append('<li class="bookmark"><span>' + bookmarks[i].title + '</span><i class="fas fa-times"></i></li>');
+        }
+    });
 }
